a1754cb2b7e5cddb1f1d8d103801f2ada1a1b997
mahdi <mahdimamashli1383@gmail.com>
2023-12-26 19:45:19 +0330

    Add userId field to Product schema

--- models/products.js
+++ models/products.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-    title : {
-        type : String,
-        required : true
-    },
-    price: {
-        type : Number,
-        required : true
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+//   userId: {
+//     type: Schema.Types.ObjectId,
+//     required: true,
+//   },
+});
 
-module.exports = mongoose.model("Product",  ProductSchema)
+module.exports = mongoose.model("Product", ProductSchema);
