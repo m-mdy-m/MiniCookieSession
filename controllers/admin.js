9d64bb05ab0cfe4295976b9baddc247c79845a84
mahdi <mahdimamashli1383@gmail.com>
2023-12-26 18:28:50 +0330

    Add product feature

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,19 @@
-exports.getAddProduct = async (req,res)=>{
-    res.render('admin/add-product', {
-        title : "add-product",
-        path : req.path,
-        editing:false
+const Product  = require('../models/products')
+exports.getAddProduct = async (req, res) => {
+  res.render("admin/add-product", {
+    title: "add-product",
+    path: req.path,
+    editing: false,
+  });
+};
+exports.postAddProduct = async(req,res)=>{
+    const title  = req.body.title
+    const price = req.body.price
+    const product = await Product.create({
+        title,
+        price
     })
+    await product.save()
+    console.log("create user", product)
+    res.redirect('/')
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
-const express=  require('express')
-const router = express.Router()
-const adminControllers = require('../controllers/admin')
-router.get('/add-product', adminControllers.getAddProduct)
+const express = require("express");
+const router = express.Router();
+const adminControllers = require("../controllers/admin");
+router.get("/add-product", adminControllers.getAddProduct);
+router.post("/add-product", adminControllers.postAddProduct);
 
-
-module.exports = router
+module.exports = router;
