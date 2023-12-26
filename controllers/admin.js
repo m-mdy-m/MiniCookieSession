a52792d40d8aa5439534770beb29cb6d88aeb5d1
mahdi <mahdimamashli1383@gmail.com>
2023-12-26 19:29:34 +0330

    Update product routes and views

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,4 @@
-const Product  = require('../models/products')
+const Product = require("../models/products");
 exports.getAddProduct = async (req, res) => {
   res.render("admin/add-product", {
     title: "add-product",
@@ -6,51 +6,55 @@ exports.getAddProduct = async (req, res) => {
     editing: false,
   });
 };
-exports.postAddProduct = async(req,res,nxt)=>{
-    const title  = req.body.title
-    const price = req.body.price
-    console.log(title);
-    console.log(price);
-    const product = await Product.create({
-        title,
-        price
-    })
-    await product.save()
-    console.log("create user", product)
-    res.redirect('/')
-}
+exports.postAddProduct = async (req, res, nxt) => {
+  const title = req.body.title;
+  const price = req.body.price;
+  console.log(title);
+  console.log(price);
+  const product = await Product.create({
+    title,
+    price,
+  });
+  await product.save();
+  console.log("create user", product);
+  res.redirect("/");
+};
 
-exports.getDsh = async (req,res)=>{
-    const products = await Product.find()
-    res.render('admin/dashboard', {
-        title : 'dashboard',
-        path : req.path,
-        products
-    })
-}
-exports.delete = async (req,res)=>{
-    const id = req.params.prodId;
-    await Product.findByIdAndDelete(id)
-    console.log("delete product")
-    return  res.redirect('/')
-}
+exports.getDsh = async (req, res) => {
+  const products = await Product.find();
+  res.render("admin/dashboard", {
+    title: "dashboard",
+    path: req.path,
+    products,
+  });
+};
+exports.delete = async (req, res) => {
+  const id = req.params.prodId;
+  await Product.findByIdAndDelete(id);
+  console.log("delete product");
+  return res.redirect("/");
+};
 
-exports.getEdit = async ( req,res)=>{
-    const id = req.params.prodId
-    const editing = req.query.edit
-    const product = await Product.findById(id)
-    res.render('admin/add-product', {
-        title : 'updatePro',
-        path: req.path,
-        product,
-        editing,
-    })
-}
-exports.postEdit = async (req,res)=>{
-    const title = req.body.title
-    const price = req.body.price
-    const id = req.params.prodId
-    console.log(id)
-    console.log(title)
-    console.log(price)
-}
+exports.getEdit = async (req, res) => {
+  const id = req.params.prodId;
+  const editing = req.query.edit;
+  const product = await Product.findById(id);
+  res.render("admin/add-product", {
+    title: "updatePro",
+    path: req.path,
+    product,
+    editing,
+  });
+};
+exports.postEdit = async (req, res) => {
+  const title = req.body.title;
+  const price = req.body.price;
+  const id = req.params.prodId;
+  const product = await Product.findByIdAndUpdate(id, {
+    title,
+    price,
+  });
+  console.log('update user');
+  await product.save();
+  return res.redirect("/");
+};
