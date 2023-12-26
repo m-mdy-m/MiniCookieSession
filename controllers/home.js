e02e3ad223bddc73c0ba45fe3a8e22c7a5995c69
mahdi <mahdimamashli1383@gmail.com>
2023-12-26 18:50:00 +0330

    Added Product model and rendered products on the home page.

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,9 @@
+const Product = require('../models/products')
 exports.getHome = async ( req,res,nxt)=>{
+    const products = await Product.find()
     res.render('home', {
         title : "home",
-        path : req.path
+        path : req.path,
+        products,
     })
 }
